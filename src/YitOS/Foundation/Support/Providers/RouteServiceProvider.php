e93eb5c2f85b586de6040b64aa9100b423dd0590
yiller <yiller@live.com>
2015-05-12 10:03:09 +0800

    modify RouteServiceProvider
    modify RouteServiceProvider for loading config

--- src/YitOS/Foundation/Support/Providers/RouteServiceProvider.php
+++ src/YitOS/Foundation/Support/Providers/RouteServiceProvider.php
@@ -4,19 +4,18 @@ use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider {
-
-	protected function loadRoutes() {
-		$this->app->call([$this, 'backend']);
+  
+  protected function loadRoutes() {
+    $this->app->call([$this, 'backend']);
     parent::loadRoutes();
-	}
+  }
   
   public function backend(Router $router) {
-    $router->group(['prefix' => 'cpanel'], function($router)
-		{
-			Route::get('/', function(){
-        return 'cpanel';
+    $router->group(['namespace' => 'YitOS\Backend\Http\Controllers', 'prefix' => config('backend.prefix')], function($router) {
+      Route::get('/', function(){
+        return 'Backend';
       });
-		});
+    });
   }
-
+  
 }
