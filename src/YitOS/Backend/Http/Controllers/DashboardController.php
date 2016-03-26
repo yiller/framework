e9a76ce2094d40b6196053a0bf4e08e787a58726
YiTao <yiller@live.com>
2016-03-26 16:34:25 +0800

    清理
    清理一

--- src/YitOS/Backend/Http/Controllers/DashboardController.php
+++
@@ -1,9 +0,0 @@
-<?php namespace YitOS\Backend\Http\Controllers;
-
-class DashboardController extends Controller {
-  
-  public function index() {
-    return view('yitos.backend.dashboard');
-  }
-  
-}
